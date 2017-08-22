87e8974ebe6071fb989040469c01aad0d44d08cb
yutao <yutao@DESKTOP-J9JILOO>
2017-08-22 13:56:52 +0800

    iview

--- www/src/main.js
+++ www/src/main.js
@@ -1,11 +1,42 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import App from './App.vue'
+import Vue from 'vue';
+import iView from 'iview';
+import VueRouter from 'vue-router';
+import Routers from './router';
 
-Vue.use(ElementUI)
+import Util from './libs/util';
+import App from './app.vue';
+import 'iview/dist/styles/iview.css';
+
+
+Vue.use(VueRouter);
+
+
+Vue.use(iView);
+
+
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach(() => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
